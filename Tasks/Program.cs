a1b2628a634e3baa4da634219fabb226a1c4599f
ArturIshmaev <arturishmaev@yandex.ru>
2022-09-17 18:00:25 +0500

    second task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -5,9 +5,36 @@ System.Console.WriteLine("Введите второе число :");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if(num1 > num2)
 {
-    System.Console.WriteLine("Максимальное число : " + num1);
+   System.Console.WriteLine("Максимальное число : " + num1);
 }
 else
 {
     System.Console.WriteLine("Максимальное число : " + num2);
 }
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число :");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2)
+{
+    System.Console.WriteLine("Ваше максимальное число : " + num1);
+}
+else if (num2 > num3)
+{
+    System.Console.WriteLine("Ваше максимальное число : " + num2);
+}
+else if (num3 > num1)
+{
+    System.Console.WriteLine("Ваше максимальное число : " + num3);
+}
+
+
+
